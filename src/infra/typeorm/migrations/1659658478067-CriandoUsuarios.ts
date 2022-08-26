b952aa9c2b7884ae9dc3dbe4311f8a5909a54bf6
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-08-25 21:06:40 -0300

    ajuste cadastro

--- src/infra/typeorm/migrations/1659658478067-CriandoUsuarios.ts
+++ src/infra/typeorm/migrations/1659658478067-CriandoUsuarios.ts
@@ -4,35 +4,14 @@ export class CriandoUsuarios1659658478067 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "tipo_perfil",
-        columns: [
-          { name: "id", type: "serial", isPrimary: true },
-          { name: "descricao", type: "varchar" },
-        ],
-      })
-    );
-
-    await queryRunner.createTable(
-      new Table({
         name: "usuarios",
         columns: [
           { name: "id", type: "uuid", isPrimary: true },
           { name: "nome", type: "varchar" },
           { name: "email", type: "varchar", isUnique: true },
           { name: "senha", type: "varchar" },
-          { name: "bio", type: "varchar" },
-          { name: "tipo_perfil_id", type: "serial" },
+          { name: "bio", type: "varchar", isNullable: true },
           { name: "criado_em", type: "timestamp", default: "now()" },
-        ],
-        foreignKeys: [
-          {
-            name: "FKTipoPerfil",
-            referencedTableName: "tipo_perfil",
-            referencedColumnNames: ["id"],
-            columnNames: ["tipo_perfil_id"],
-            onDelete: "CASCADE",
-            onUpdate: "CASCADE",
-          },
         ],
       })
     );
@@ -40,6 +19,5 @@ export class CriandoUsuarios1659658478067 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropTable("usuarios");
-    await queryRunner.dropTable("tipo_perfil");
   }
 }
