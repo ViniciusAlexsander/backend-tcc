50fdae901f369775e624ce41d2608ba70df6df9a
vinicius alexsander <viniciusalexsander1@gmail.com>
2022-10-09 01:14:10 -0300

    voltando cors

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
 import "express-async-errors";
 
 import createConnection from "../infra/typeorm";
@@ -12,21 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-});
+app.use(cors());
 
 app.use(router);
 
